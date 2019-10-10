565223c4cc2d3c5cc4301b211917fa23bd7e70f1
fjb <fedejbrun@gmail.com>
2019-10-10 18:32:56 -0300

    First attempt at trying to use Recycler View (Couldn't Finish so week forecaste remains implementes as a static list)

---
+++ app/src/main/java/com/example/theweatherguy/RecyclerViewItem.java
@@ -0,0 +1,25 @@
+package com.example.theweatherguy;
+
+public class RecyclerViewItem {
+    private int mAnimation;
+    private String mText1;
+    private String mText2;
+
+    public RecyclerViewItem(int imageResource, String text1, String text2) {
+        mAnimation = imageResource;
+        mText1 = text1;
+        mText2 = text2;
+    }
+
+    public int getImageResource() {
+        return mAnimation;
+    }
+
+    public String getText1() {
+        return mText1;
+    }
+
+    public String getText2() {
+        return mText2;
+    }
+}
--- app/src/main/java/com/example/theweatherguy/TodayWeather.java
+++ app/src/main/java/com/example/theweatherguy/TodayWeather.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +15,8 @@ public class TodayWeather extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Toast toast = Toast.makeText(getActivity(),getString(R.string.under_construction),Toast.LENGTH_SHORT);
+        toast.show();
         return inflater.inflate(R.layout.fragment_today_weather, container, false);
     }
 }
